f35f78b79c3d9864853115b1a2dc909da075032e
Matias Aguilar Araya <AGUILARM@farmacorp.com>
2024-10-09 16:51:00 -0400

    add extra feature in capital page

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, map, Observable, of } from 'rxjs';
+import { catchError, delay, map, Observable, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({ providedIn: 'root' })
@@ -9,6 +9,14 @@ export class CountriesService {
   private apiUrl: string = 'https://restcountries.com/v3.1'
 
   constructor(private http: HttpClient) { }
+
+  private getCountriesRequest(url:string):Observable<Country[]>{
+    return this.http.get<Country[]>(url)
+      .pipe(
+        catchError(error => of([])),
+        delay(2000),
+      );
+  }
 
   searchCountryByAlphaCode(code: string): Observable<Country|null> {
     return this.http.get<Country[]>(`${this.apiUrl}/alpha/${code}`)
@@ -19,11 +27,8 @@ export class CountriesService {
   }
 
   searchCapital(term: string): Observable<Country[]> {
-
-    return this.http.get<Country[]>(`${this.apiUrl}/capital/${term}`)
-      .pipe(
-        catchError(error => of([]))
-      );
+    const url = `${this.apiUrl}/capital/${term}`;
+    return this.getCountriesRequest(url);
   }
 
   searchCountry(term: string): Observable<Country[]> {
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,15 +6,17 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { RouterModule } from '@angular/router';
 import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { LoadingSpinnerComponent } from './components/loading-spinner/loading-spinner.component';
 
 
 
 @NgModule({
   declarations: [
-    HomePageComponent,
     AboutPageComponent,
-    SidebarComponent,
     ContactPageComponent,
+    HomePageComponent,
+    LoadingSpinnerComponent,
+    SidebarComponent,
     SearchBoxComponent,
   ],
   imports: [
@@ -22,10 +24,11 @@ import { SearchBoxComponent } from './components/search-box/search-box.component
     RouterModule
   ],
   exports: [
-    HomePageComponent,
     AboutPageComponent,
-    SidebarComponent,
     ContactPageComponent,
+    HomePageComponent,
+    LoadingSpinnerComponent,
+    SidebarComponent,
     SearchBoxComponent
   ]
 })
